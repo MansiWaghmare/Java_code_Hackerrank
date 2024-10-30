e9ccd1d9d305bec334f88a1e0a7ab59fcaeed977
Mansi <121502548+MansiWaghmare@users.noreply.github.com>
2024-10-30 11:42:01 +0530

    Updated If-else.java

--- If-else.java
+++ If-else.java
@@ -1,14 +1,14 @@
 /*Given an integer, , perform the following conditional actions:
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Complete the stub code provided in your editor to print whether or not  is weird.
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not n is weird.
 
 INPUT FORMAT
-A single line containing a positive integer, .
+A single line containing a positive integer,n .
 Constraints
-
+1<=n<=100
 OUTPUT FORMAT
 Print Weird if the number is weird; otherwise, print Not Weird.
 Sample Input 0
@@ -21,10 +21,10 @@ Sample Output 1
 Not Weird
 
 EXPLAINATION
-Sample Case 0:  
- is odd and odd numbers are weird, so we print Weird.
-Sample Case 1:  
- and  is even, so it isn't weird. Thus, we print Not Weird.*/
+Sample Case 0: n=3
+n is odd and odd numbers are weird, so we print Weird.
+Sample Case 1: n=24
+n>20 and n is even, so it isn't weird. Thus, we print Not Weird.*/
 
 SOLUTION/CODE:
 import java.util.*;
